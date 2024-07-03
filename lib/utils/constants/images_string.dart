9ddc50ed922d3c43fd24cd45224212ebd2613cf2
mudakir afridi <mudakkirafridi@gmail.com>
2024-07-03 16:45:45 -0700

    first

---
+++ lib/utils/constants/images_string.dart
@@ -0,0 +1,10 @@
+
+class AppImages{
+  AppImages._();
+  
+  static const String darkAppLogo = 'assets/logos/black_logo.png';
+  static const String lightAppLogo = 'assets/logos/white_logo.png';
+
+  // social media logo 
+  static const String googleLogo = 'assets/logos/google_logo.webp';
+}
---
+++ lib/utils/constants/text_string.dart
@@ -0,0 +1,20 @@
+
+class AppTexts{
+  AppTexts._();
+
+  // Global texts
+
+  // onboarding texts
+  static const String onBoardingtitle1 = 'Choose Your Product';
+   static const String onBoardingtitle2 = 'Select Payment Method';
+    static const String onBoardingtitle3 = 'Deliver At Your Door Step';
+
+    static const String onBoardingSubtitle1 = 'Welcome To A World Of Limitless Choice - Your Perfect Product Awaits!';
+   static const String onBoardingSubtitle2 = 'Your Seamless Transactions, Choose Your Payment Path - Your Convenience, Our Priority!';
+    static const String onBoardingSubtitle3 = 'From Our Doorstep To Yours - Swift, Secure, And Contacless Delivery!';
+
+// home sreen
+static const String homeAppbarTitle = 'Good Day For Shopping';
+   static const String homeAppbarSubtitle = 'Mudakkir Afridi,';
+   
+}
