65c3a87362ed42f888a315de78c5f0f79b394842
mudakir afridi <mudakkirafridi@gmail.com>
2024-07-04 18:23:23 -0700

    complete flutter folder structure

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:professional_e_commerce/base.dart';
 
-import 'package:professional_e_commerce/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
