bc056ba077086e5a1c72275cddc82ba57b5e78d8
mudakir afridi <mudakkirafridi@gmail.com>
2024-08-22 03:58:52 -0700

    bottom nav complete

--- lib/features/authentication/screen/login_screen/widgets/login_form.dart
+++ lib/features/authentication/screen/login_screen/widgets/login_form.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:professional_e_commerce/features/authentication/screen/password_configuration/forgot_password.dart';
 import 'package:professional_e_commerce/features/authentication/screen/sign_screen/sign_up.dart';
+import 'package:professional_e_commerce/navigation_menu.dart';
 import 'package:professional_e_commerce/utils/constants/sizes.dart';
 import 'package:professional_e_commerce/utils/constants/text_strings.dart';
 
@@ -61,7 +62,7 @@ class LoginForm extends StatelessWidget {
           SizedBox(
               width: double.infinity,
               child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () => Get.to(()=> const NavigationMenu()),
                   child: const Text(AppTexts.signIn))),
           // create account button
           const SizedBox(
